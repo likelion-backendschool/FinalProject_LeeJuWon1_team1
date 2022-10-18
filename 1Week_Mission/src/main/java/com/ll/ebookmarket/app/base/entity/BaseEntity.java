504b2a58b4dcc9e1737ef1ebb544625b9f3bbf40
nunusmohum <halojw301@gmail.com>
2022-10-18 09:38:51 +0900

    chore: build.gradle 의존성 추추가

---
+++ 1Week_Mission/src/main/java/com/ll/ebookmarket/app/base/entity/BaseEntity.java
@@ -0,0 +1,22 @@
+package com.ll.ebookmarket.app.base.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+
+import static lombok.AccessLevel.PROTECTED;
+
+@Getter
+@SuperBuilder
+@MappedSuperclass
+@NoArgsConstructor(access = PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+@ToString
+public class BaseEntity {
+
+}
