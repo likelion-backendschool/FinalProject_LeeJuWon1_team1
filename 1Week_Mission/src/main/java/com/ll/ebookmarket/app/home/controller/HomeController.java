c8c0d3a9b6da1883eb6c2a558cd27e05562e1ddf
nunusmohum <halojw301@gmail.com>
2022-10-18 15:29:57 +0900

    feat: layout & html 베이스 생성

---
+++ 1Week_Mission/src/main/java/com/ll/ebookmarket/app/home/controller/HomeController.java
@@ -0,0 +1,10 @@
+package com.ll.ebookmarket.app.home.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+public class HomeController {
+
+}
