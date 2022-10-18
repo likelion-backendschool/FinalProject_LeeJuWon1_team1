ed35bb0e0026eac67506d60268d6dfb52f05a84c
nunusmohum <halojw301@gmail.com>
2022-10-18 15:34:25 +0900

    chore: gitignore 추가, init.sql 생성

--- 1Week_Mission/src/main/java/com/ll/ebookmarket/app/home/controller/HomeController.java
+++ 1Week_Mission/src/main/java/com/ll/ebookmarket/app/home/controller/HomeController.java
@@ -2,9 +2,14 @@ package com.ll.ebookmarket.app.home.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
 
+    @GetMapping("/")
+    public String showMain() {
+        return "home/main";
+    }
 }
