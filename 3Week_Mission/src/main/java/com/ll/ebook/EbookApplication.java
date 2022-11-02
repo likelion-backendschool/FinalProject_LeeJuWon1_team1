b68b99691c5791d7a97bbd7a70c23650aae8883f
nunusmohum <halojw301@gmail.com>
2022-11-02 10:35:25 +0900

    feat: 정산 배치 기반 코드 작성

--- 3Week_Mission/src/main/java/com/ll/ebook/EbookApplication.java
+++ 3Week_Mission/src/main/java/com/ll/ebook/EbookApplication.java
@@ -1,5 +1,6 @@
 package com.ll.ebook;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -9,6 +10,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
+@EnableBatchProcessing
 @EnableJpaAuditing
 @EnableAsync
 public class EbookApplication {
